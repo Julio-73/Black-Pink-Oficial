c9feb96cea046b8754598556d51427ff32170134
julio-cesar <juliocesarquispegarrido@gmail.com>
2023-03-19 22:51:13 -0500

    nuevo proyecto

---
+++ page.js
@@ -0,0 +1,75 @@
+// SCROLL INTEGRANTES
+
+ScrollReveal({
+     reset: true ,
+     distance: '80px',
+    duration:1000 ,
+  
+    delay: 100,
+    
+    });
+
+
+    ScrollReveal().reveal('.suscrebe', { origin: 'top' });
+
+// scroll video
+
+
+
+    ScrollReveal({
+     reset: true ,
+     distance: '80px',
+    duration:1000 ,
+  
+    delay: 100,
+    
+    });
+
+
+    ScrollReveal().reveal('.poster', { origin: 'top' });
+
+
+
+    // scroll listem
+
+
+
+    ScrollReveal({
+     reset: true ,
+     distance: '80px',
+    duration:1000 ,
+  
+    delay: 100,
+    
+    });
+
+
+    ScrollReveal().reveal('.bebe' ,{ origin: 'top' });
+
+
+// Menu
+
+
+    ScrollReveal({
+        reset: true ,
+        distance: '5px',
+       duration:1000 ,
+     
+       delay: 100,
+       
+       });
+   
+   
+       ScrollReveal().reveal('.julio' ,{ origin: 'bottom' });
+
+       ScrollReveal({
+        reset: true ,
+        distance: '5px',
+       duration:1000 ,
+     
+       delay: 100,
+       
+       });
+   
+   
+       ScrollReveal().reveal('.sociales a' ,{ origin: 'left' });
